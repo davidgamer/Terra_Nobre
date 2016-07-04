701e707e9dedd9792b4b472d48c062bb4d43bb9a
david henrique <davidhenriquesi@live.com>
2016-07-04 15:59:01 -0300

    grrrr

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,37 +1,4 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-<!DOCTYPE html>
 
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Terra Nobre</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link rel="stylesheet" href= <?php echo base_url('includes/css/bootstrap.min.css') ?> type="text/css">
-
-    <!-- Custom Fonts -->
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href=<? echo base_url('includes/font-awesome/css/font-awesome.min.css')?> type="text/css">
-	 <link rel="stylesheet" href=<? echo base_url('includes/css/style.css')?> type="text/css">
-
-    <!-- Plugin CSS -->
-    <link rel="stylesheet" href=<? echo base_url('includes/css/magnific-popup.css') ?> type="text/css">
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href=<? echo base_url('includes/css/creative.css')?> type="text/css">
-
-
-
-</head>
 
 
 
@@ -131,8 +98,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <script src=<?echo base_url('includes/js/jquery.fittext.js')?>></script>
     <script src=<?echo base_url('includes/js/jquery.magnific-popup.min.js')?>></script>
 
-    <!-- Custom Theme JavaScript -->
-    <script src=<?echo base_url('includes/js/creative.js')?>></script>
+    
 
 </body>
 
