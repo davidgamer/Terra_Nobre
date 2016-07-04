c06be89837946780d1296715241bdc146b622fc8
david henrique <davidhenriquesi@live.com>
2016-07-04 16:05:03 -0300

    ta chegando

---
+++ application/views/view_contact.php
@@ -0,0 +1,40 @@
+  <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Entre em  Contato</h2>
+                    <hr class="primary">
+                    <p></p>
+                </div>
+                <div class="col-lg-4 col-lg-offset-2 text-center">
+                    <i class="fa fa-phone fa-3x sr-contact"></i>
+                    <p>Telefone</p>
+                    <p>(34)3825-4343</p>
+                </div>
+                <div class="col-lg-4 text-center">
+                    <i class="fa fa-envelope-o fa-3x sr-contact"></i>
+                    <p>Email</p>
+                    <p><a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a></p>
+                </div>
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                  <i class="fa fa-location-arrow fa-3x sr-contact"></i>
+                    <p>Endereço</p>
+                    <p>Rua Dona Luisa, 328 Patos De Minas</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- jQuery -->
+    <script src=<?echo base_url('includes/js/jquery.js')?>></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src=<?echo base_url('includes/js/bootstrap.min.js')?>></script>
+
+    <!-- Plugin JavaScript -->
+    <script src=<?echo base_url('includes/js/scrollreveal.min.js')?></script>
+    <script src=<?echo base_url('includes/js/jquery.easing.min.js')?>></script>
+    <script src=<?echo base_url('includes/js/jquery.fittext.js')?>></script>
+    <script src=<?echo base_url('includes/js/jquery.magnific-popup.min.js')?>></script>
+
+    
---
+++ application/views/view_ini.php
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+    <header class="text-center">
+        <div class="header-content">
+            <div class="header-content-inner">
+			<br />
+                <h1>Terra Nobre</h1>
+                <hr>
+                <p>Compra, Venda e Administração de imoveis</p>
+                <a href="#" class="btn btn-primary " >Conheça Nossos Serviços</a>
+				
+            </div>
+        </div>
+    </header>
+
+
+    <section id="novos">
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-8 col-lg-offset-2 text-center">
+          <h2 class="section-heading">Novos Imovéis</h2>
+
+
+      </div>
+
+    </div>
+  </div>
+
+    </section>
+    
+  
+ 
+
