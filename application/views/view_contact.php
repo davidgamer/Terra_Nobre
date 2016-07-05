c44d713f5eacb71325132c4dc5b2cb030cefa191
david henrique <davidhenriquesi@live.com>
2016-07-05 13:44:17 -0300

    cdn é vida

--- application/views/view_contact.php
+++ application/views/view_contact.php
@@ -25,16 +25,6 @@
         </div>
     </section>
 
-    <!-- jQuery -->
-    <script src=<?echo base_url('includes/js/jquery.js')?>></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src=<?echo base_url('includes/js/bootstrap.min.js')?>></script>
-
-    <!-- Plugin JavaScript -->
-    <script src=<?echo base_url('includes/js/scrollreveal.min.js')?></script>
-    <script src=<?echo base_url('includes/js/jquery.easing.min.js')?>></script>
-    <script src=<?echo base_url('includes/js/jquery.fittext.js')?>></script>
-    <script src=<?echo base_url('includes/js/jquery.magnific-popup.min.js')?>></script>
+   
 
     
--- application/views/view_header.php
+++ application/views/view_header.php
@@ -14,21 +14,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     <title>Terra Nobre</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link rel="stylesheet" href= <?php echo base_url('includes/css/bootstrap.min.css') ?> type="text/css">
-    <link rel="stylesheet" href=<? echo base_url('includes/css/style.css')?> type="text/css">
+
 
     <!-- Custom Fonts -->
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href=<? echo base_url('includes/font-awesome/css/font-awesome.min.css')?> type="text/css">
-	
+    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css' rel='stylesheet' type="text/css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.0.0.slim.min.js" integrity="sha256-Rf4BadfyCtsvHmO89BUZcbYvNNvZvOT08ALfEzvCsD0=" crossorigin="anonymous"></script>
 
-    <!-- Plugin CSS -->
-    <link rel="stylesheet" href=<? echo base_url('includes/css/magnific-popup.css') ?> type="text/css">
 
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href=<? echo base_url('includes/css/creative.css')?> type="text/css">
 
 
 
