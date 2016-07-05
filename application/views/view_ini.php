ecd123d30820e14e5d6d052a8780ed5086f6d3c3
david henrique <davidhenriquesi@live.com>
2016-07-05 15:42:34 -0300

    quase

--- application/views/view_ini.php
+++ application/views/view_ini.php
@@ -3,31 +3,19 @@
         <div class="header-content">
             <div class="header-content-inner">
 			<br />
-        <div id="backimage">
+        
                 <h1>Terra Nobre</h1>
                 <hr>
+                <div id="ini">
                 <p>Compra, Venda e Administração de imoveis</p>
                 <a href="#" class="btn btn-primary " >Conheça Nossos Serviços</a>
 				
+                </div>
             </div>
         </div>
     </header>
 
 
-<section id="novos">
-
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-lg-8 col-lg-offset-2 text-center">
-          <h2 class="section-heading">Novos Imovéis</h2>
-
-
-      </div>
-
-    </div>
-  </div>
-
-    </section>
     </div>
   
  
