3c698c36e765423a755377969b4a839c0cea0bf5
david henrique <davidhenriquesi@live.com>
2016-07-06 17:24:53 -0300

    merrrda

--- application/views/view_content.php
+++ application/views/view_content.php
@@ -6,8 +6,8 @@
 <!--- Colocar Resultado do BD ness formato -->
 
 
-    <div class="row">
-        <div class="col-sm-6 col-md-4">
+<div class="row">
+    <div class="col-sm-6 col-md-4">
         <div id="obj">
             <div class="thumbnail">
                 <img src="http://www.donagiraffa.com/wp-content/uploads/2012/12/Fachada-de-casa-06.jpg" alt="...">
@@ -16,31 +16,19 @@
                     <p>...</p>
                     <p>
 
-                       
+
 
                         <a href="#" class="btn btn-success" role="button">Ver mais</a>
 
                     </p>
                 </div>
             </div>
-            <!--- -->
 
-            <div class="thumbnail">
-                <img src="http://www.donagiraffa.com/wp-content/uploads/2012/12/Fachada-de-casa-06.jpg" alt="...">
-                <div class="caption">
-                    <h3>Thumbnail label</h3>
-                    <p>...</p>
-                    <p>
 
-                       
 
-                        <a href="#" class="btn btn-success" role="button">Ver mais</a>
-
-                    </p>
-                </div>
-            </div>
         </div>
+
+
     </div>
 
 </div>
-
