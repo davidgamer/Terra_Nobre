4a78299599dd974e653a4dec3747e367c6f39673
david henrique <davidhenriquesi@live.com>
2016-07-06 17:11:56 -0300

    agora sim kk

--- application/views/view_content.php
+++ application/views/view_content.php
@@ -1,5 +1,27 @@
 <div class="jumbotron">
   <h1>Imoveis</h1>
   <p></p>
-  <p><a class="btn btn-primary btn-lg">Learn more</a></p>
+  
+</div>
+<div id="obj">
+
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="..." alt="...">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p>...</p>
+        <p>
+        
+        <a href="#" class="btn btn-primary" role="button">Button</a> 
+        
+        <a href="#" class="btn btn-default" role="button">Button</a>
+        
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
 </div>
--- application/views/view_ini.php
+++ application/views/view_ini.php
@@ -1,19 +1,20 @@
 
-    <header class="text-center">
-        <div class="header-content">
+   
+        
             <div class="header-content-inner">
 			<br />
-        
+        <div class="jumbotron">
                 <h1>Terra Nobre</h1>
                 <hr>
                 <div id="ini">
                 <p>Compra, Venda e Administração de imoveis</p>
-                <a href="<?php echo site_url("imoveis");?>" class="btn btn-primary " >Conheça Nossos Serviços</a>
+                <a href="<?php echo site_url("imoveis");?>" class="btn btn-primary btn-lg" >Conheça Nossos Serviços</a>
 				
                 </div>
             </div>
-        </div>
-    </header>
+            
+        
+   
 
 
     </div>
