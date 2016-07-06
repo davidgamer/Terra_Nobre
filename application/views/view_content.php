380960c66a6e2bdbd26636035454920e25e438d3
david henrique <davidhenriquesi@live.com>
2016-07-06 17:22:38 -0300

    quase kk

--- application/views/view_content.php
+++ application/views/view_content.php
@@ -4,12 +4,13 @@
 
 </div>
 <!--- Colocar Resultado do BD ness formato -->
-<div id="obj">
+
 
     <div class="row">
         <div class="col-sm-6 col-md-4">
+        <div id="obj">
             <div class="thumbnail">
-                <img src="..." alt="...">
+                <img src="http://www.donagiraffa.com/wp-content/uploads/2012/12/Fachada-de-casa-06.jpg" alt="...">
                 <div class="caption">
                     <h3>Thumbnail label</h3>
                     <p>...</p>
@@ -17,7 +18,23 @@
 
                        
 
-                        <a href="#" class="btn btn-success" role="button">Button</a>
+                        <a href="#" class="btn btn-success" role="button">Ver mais</a>
+
+                    </p>
+                </div>
+            </div>
+            <!--- -->
+
+            <div class="thumbnail">
+                <img src="http://www.donagiraffa.com/wp-content/uploads/2012/12/Fachada-de-casa-06.jpg" alt="...">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>...</p>
+                    <p>
+
+                       
+
+                        <a href="#" class="btn btn-success" role="button">Ver mais</a>
 
                     </p>
                 </div>
@@ -26,3 +43,4 @@
     </div>
 
 </div>
+
