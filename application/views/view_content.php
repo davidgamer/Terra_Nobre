93597b6e89aaa5408453126423e10d281587d4b8
david henrique <davidhenriquesi@live.com>
2016-07-06 17:13:23 -0300

    quase la agora

--- application/views/view_content.php
+++ application/views/view_content.php
@@ -1,27 +1,28 @@
 <div class="jumbotron">
-  <h1>Imoveis</h1>
-  <p></p>
-  
+    <h1>Imoveis</h1>
+    <p></p>
+
 </div>
+<!--- Colocar Resultado do BD ness formato -->
 <div id="obj">
 
-<div class="row">
-  <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <img src="..." alt="...">
-      <div class="caption">
-        <h3>Thumbnail label</h3>
-        <p>...</p>
-        <p>
-        
-        <a href="#" class="btn btn-primary" role="button">Button</a> 
-        
-        <a href="#" class="btn btn-default" role="button">Button</a>
-        
-        </p>
-      </div>
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="..." alt="...">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>...</p>
+                    <p>
+
+                        <a href="#" class="btn btn-primary" role="button">Button</a>
+
+                        <a href="#" class="btn btn-default" role="button">Button</a>
+
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 
 </div>
